f3c23b8dbf68f90b3b6c38fcbacc9c1ec9b4199a
Stevie Gill <steviexgill@gmail.com>
2020-11-03 14:51:24 -0500

    API call & text display working

--- index.js
+++ index.js
@@ -1,41 +1,19 @@
 $(document).ready(function() {
-  console.log('Hello World!')
+  const newQuote = () => {
+    let quote // data.content
+    let author // data.author
 
-  fetch('https://api.quotable.io/random')
-    .then(response => response.json())
-    .then(data => console.log(data))
-
-  function genQuote() {
-    $.ajax({
-      url: 'https://api.forismatic.com/api/1.0/', // base url
-      jsonp: 'jsonp', // sets the callback name for jsonp datatype (below)
-      dataType: 'jsonp', // adds jsonp=? instead of callback=?
-      data: {
-        // these get added to the end of the URL as parameters
-        method: 'getQuote', // adds method=getQuote
-        lang: 'en', // adds lang=en
-        format: 'jsonp' // add format=jsonp
-      }
-      // so full URL = https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?
-    })
-      .done(function(data) {
-        quote = data.quoteText
-        console.log(data)
-        quoteUrl = data.quoteLink
-        if (!data.quoteAuthor) {
-          // in case no author is named (emtpy string is falsy value)
-          author = 'Anonymous'
-        } else {
-          author = data.quoteAuthor
-        }
-        $('.quote').html(data.quoteText)
-        $('.author').html('&#8212\xa0\xa0' + author)
+    console.log('Hello World!')
+    fetch('https://api.quotable.io/random')
+      // only a successful request will supply a response.ok
+      .then(response => (response.ok ? response.json() : console.log('balls')))
+      .then(data => {
+        console.log(data.content)
+        $('#text').html(data.content)
+        $('#author').html('&#8212\xa0\xa0' + data.author)
       })
-      .fail(function(err) {
-        console.log('Error: ' + err.status)
-        $('.quote').html('Whoops... try again!') // in case json doesn't load
-      })
+      .catch(error => console.log(error)) // only rejects on network errors (see above)
   }
 
-  // genQuote()
+  newQuote()
 })
