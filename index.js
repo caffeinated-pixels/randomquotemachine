775d656898956ec3e84040e91f369248fb977f3d
Stevie Gill <steviexgill@gmail.com>
2020-11-03 16:30:43 -0500

    hooked up buttons

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 $(document).ready(function() {
   const newQuote = () => {
-    let quote // data.content
-    let author // data.author
-
-    console.log('Hello World!')
     fetch('https://api.quotable.io/random')
       // only a successful request will supply a response.ok
       .then(response => (response.ok ? response.json() : console.log('balls')))
       .then(data => {
-        console.log(data.content)
         $('#text').html(data.content)
-        $('#author').html('&#8212\xa0\xa0' + data.author)
+        $('#author').html(`&#8212\xa0\xa0${data.author}`)
       })
       .catch(error => console.log(error)) // only rejects on network errors (see above)
   }
 
-  newQuote()
+  newQuote() // automatically retrieves first random quote from API
+
+  $('#new-quote').on('click', () => newQuote()) // get new quote from api
+  $('#tweet-quote').on('click', () => {
+    const quote = $('#text').text()
+    const author = $('#author').text()
+
+    const twtLink =
+      'http://twitter.com/intent/tweet?text=' +
+      encodeURIComponent(`"${quote}"  ${author}`)
+    // encodeURIComponent('alan')
+    window.open(twtLink, '_blank')
+  })
 })
