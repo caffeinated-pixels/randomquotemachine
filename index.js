e4a0c7cc31a4fa52113c8ef8a4b767092331d362
Stevie Gill <steviexgill@gmail.com>
2020-11-04 14:26:34 -0500

    added fetch error msg

--- index.js
+++ index.js
@@ -15,50 +15,48 @@ $(document).ready(function() {
     'rgb(44, 19, 255)'
   ]
 
-  const newQuote = () => {
+  const getQuote = () => {
+    //I though it would be fun to include some error handling in the fetch request
+    const errorMsg = updateText('Opps something went wrong', '')
+
     fetch('https://api.quotable.io/random')
       // only a successful request will supply a response.ok
-      .then(response => (response.ok ? response.json() : console.log('balls')))
-      .then(data => {
-        //select random color without repeating previous color
-        const oldColor = $('#text').css('color') // get prev color (always returns RGB)
-        const updatedColors = [...colors].filter(item => item !== oldColor) // remove prev col
-        const color = Math.floor(Math.random() * updatedColors.length) // random color
-
-        // display quote and author on page using fade animations
-        $('#text').fadeOut(750, () => {
-          $('#text')
-            .html(data.content)
-            .css('color', updatedColors[color]) // change color
-          $('#text').fadeIn(1000)
-        }) // need to use callback so it waits for fadeOut to finish!
-
-        $('#author').fadeOut(750, () => {
-          $('#author').html(`&#8212\xa0\xa0${data.author}`)
-          $('#author').fadeIn(3000)
-        }) // need to use callback so it waits for fadeOut to finish!
-
-        // update href of tweet button
-        $('#tweet-quote').attr(
-          'href',
-          'http://twitter.com/intent/tweet?text=' +
-            encodeURIComponent(`"${data.content}" — ${data.author}`)
-        )
-      })
-      .catch(error => console.log(error)) // only rejects on network errors (see above)
+      .then(response => (response.ok ? response.json() : errorMsg))
+      .then(data => (data ? updateText(data.content, data.author) : null))
+      .catch(error => {
+        console.log(error)
+        errorMsg
+      }) // catch only rejects on network errors (see above)
   }
 
-  newQuote() // automatically retrieves first random quote from API
+  const updateText = (text, author) => {
+    //select random color without repeating previous color
+    const oldColor = $('#text').css('color') // get prev color (always returns RGB)
+    const updatedColors = [...colors].filter(item => item !== oldColor) // remove prev col
+    const color = Math.floor(Math.random() * updatedColors.length) // random color
 
-  $('#new-quote').on('click', () => newQuote()) // get new quote from api
-  // $('#tweet-quote').on('click', () => {
-  //   const quote = $('#text').text()
-  //   const author = $('#author').text()
-  //
-  //   const twtLink =
-  //     'http://twitter.com/intent/tweet?text=' +
-  //     encodeURIComponent(`"${quote}"  ${author}`)
-  //   // encodeURIComponent('alan')
-  //   window.open(twtLink, '_blank')
-  // })
+    // display quote and author on page using fade animations
+    $('#text').fadeOut(750, () => {
+      $('#text')
+        .html(text)
+        .css('color', updatedColors[color]) // change color
+      $('#text').fadeIn(1000)
+    }) // need to use callback so it waits for fadeOut to finish!
+
+    $('#author').fadeOut(750, () => {
+      $('#author').html(`&#8212\xa0\xa0${author}`)
+      $('#author').fadeIn(3000)
+    }) // need to use callback so it waits for fadeOut to finish!
+
+    // update href of tweet button
+    $('#tweet-quote').attr(
+      'href',
+      'http://twitter.com/intent/tweet?text=' +
+        encodeURIComponent(`"${text}" — ${author}`)
+    )
+  }
+
+  getQuote() // automatically retrieves first random quote from API
+
+  $('#new-quote').on('click', () => getQuote()) // get new quote from api
 })
